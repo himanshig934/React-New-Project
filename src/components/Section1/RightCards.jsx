537f3e30ec3db1f7fe02acee38742f7f401c9455
Himanshi <himanshig934@gmail.com>
2026-06-04 22:47:22 +0530

    add the files

--- src/components/Section1/RightCards.jsx
+++ src/components/Section1/RightCards.jsx
@@ -4,24 +4,38 @@ import cardimage from '../../images/gettyimages-2214991522-640x640.jpg';
 export default function RightCards({items}) {
   return (
     <>
-      <div className='relative h-full flex justify-center items-center rounded-4xl overflow-hidden'>
+      <div className='relative h-full flex justify-center items-center rounded-4xl shrink-0 overflow-hidden'>
 
         {/* Image */}
         <img src={items.URL} alt="" className='h-full w-full object-cover rounded-4xl'/>
 
         {/* Overlay */}
         <div className='absolute inset-0 bg-black/60 flex flex-col justify-between p-6 rounded-4xl'>
-
-          <h1 className='text-white text-3xl font-bold'>{items.number}</h1>
-
-          <div className='text-white'>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus alias laborum, ipsum tempore, 
-              ullam consequatur nihil consectetu quasi repellendus consequuntur!</p>
+          
+          {/* number */}
+          <div className='bg-white rounded-full h-10 w-10 justify-center items-center flex'>
+            <h1 className='text-black text-[20px] font-bold'>{items.number}</h1>
           </div>
-          <button className='text-white rounded-full' style={items.color}>submit</button>
+
+          {/* text */}
+
+          <div className='text-white leading-[28px]'>
+            <p>{items.text}</p>
+          </div>
+
+            {/* button */}
+          <div className='justify-between text-sm'>
+          <button className='text-white rounded-full px-8 py-2' style={{backgroundColor: items.color}}>{items.button}</button>
+          <button className='text-white px-4 py-2 rounded-full' style={{backgroundColor: items.color}}><i class="ri-arrow-right-line"></i></button>
         </div>
 
+        </div>
       </div>
     </>
   )
 }
+
+
+
+
+
--- src/components/Section1/RightContent.jsx
+++ src/components/Section1/RightContent.jsx
@@ -6,17 +6,33 @@ export default function RightContent() {
 const servicesdata = [
     {
       number: 1,
-      URL : 'https://images.unsplash.com/photo-1506744038136-46273834b3fb',
+      URL : 'https://plus.unsplash.com/premium_photo-1661769159995-f3af0089875f?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      button: 'Satisfied',
+      color: '#270861',
+      text: 'Tech-savvy individuals seeking seamless digital banking experiences, instant payments, budgeting tools, and personalized financial insights to manage their growing income effectively.'
     },
 
     {
       number: 2,
-      URL : 'https://images.unsplash.com/photo-1494526585095-c41746248156',
+      URL : 'https://plus.unsplash.com/premium_photo-1661767011483-feab300357ba?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      button: 'Underserved',
+      color: '#6c0b2a',
+      text: 'Families looking for secure savings plans, joint accounts, education funds, and smart financial management tools to support their future goals.'
     },
 
     {
       number: 3,
-      URL : 'https://images.unsplash.com/photo-1519125323398-675f0ddb6308',
+      URL : 'https://plus.unsplash.com/premium_photo-1661722273422-8d1723e8f905?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      button: 'Shopsy',
+      color: '#026d50',
+      text: 'Entrepreneurs who need efficient payment processing, cash-flow monitoring, business loans, and scalable banking solutions to accelerate growth.'
+    },
+    {
+      number: 4,
+      URL : 'https://plus.unsplash.com/premium_photo-1661599805048-5b5c9bdf5f44?q=80&w=626&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      button: 'Read More',
+      color: '#246d02',
+      text: 'Entrepreneurs who need efficient payment processing, cash-flow monitoring, business loans, and scalable banking solutions to accelerate growth.'
     }
   ]
 
@@ -24,7 +40,7 @@ const servicesdata = [
   return (
     
     <>
-     <div className='w-3/4 h-full p-4 flex gap-6'>
+     <div className='w-3/4 h-full p-4 flex gap-6 overflow-x-auto'>
         {servicesdata.map((items, i) => (
           <RightCards items={items} key={i} />
         ))}
